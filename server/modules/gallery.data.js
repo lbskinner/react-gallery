f2436f3025e3a6e7ce440c02f2b1e2755dc8302c
Leila Skinner <beileiwang@gmail.com>
2020-04-10 16:04:05 -0500

    added more datat to gallery data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,35 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/goat_small.jpg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: "images/desserts.jpg",
+    description: "I've got a sweet tooth.",
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: "images/hometown.jpg",
+    description: "This is my hometown, Shenyang, China.",
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: "images/hotpot.jpg",
+    description:
+      "Hotpot is my favorite food and if you haven't tried it, you are miss out on life!",
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: "images/kids.jpg",
+    description: "The most precious things in my life.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
