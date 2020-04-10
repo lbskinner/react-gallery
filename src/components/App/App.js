c472d85d9810a058ea1f023294e978ed84a43865
Leila Skinner <beileiwang@gmail.com>
2020-04-10 16:21:23 -0500

    created axios get call and works

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    galleryData: [],
+  };
+  componentDidMount() {
+    console.log("Component App Did Mount");
+    this.getGallery();
+  }
+
+  //
+  // API SERVER CALLS
+  // -------------------
+
+  getGallery() {
+    axios({
+      method: "GET",
+      url: "/gallery",
+    })
+      .then((response) => {
+        console.log("Server Response: ", response.data);
+        this.setState({
+          galleryData: [...response.data],
+        });
+      })
+      .catch((error) => {
+        console.log("GET ERROR: ", error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" alt="goat" />
       </div>
     );
   }
